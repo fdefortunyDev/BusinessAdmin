1b351de3ca1b00d422d8908bbd49c8e6f13c09e9
fdefortunyDev <fdefortuny@gmail.com>
2025-02-03 00:35:56 -0300

    feat: refresh expired tokens

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { ApiKeyStrategy } from './apikey.strategy';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
 import { HttpModule } from '@nestjs/axios';
 import { AuthController } from './auth.controller';
+import { ConfigModule } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [PassportModule, HttpModule],
+  imports: [PassportModule, HttpModule, ConfigModule],
   controllers: [AuthController],
-  providers: [ApiKeyStrategy, JwtStrategy, AuthService],
+  providers: [ApiKeyStrategy, AuthService, JwtStrategy],
+  exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule {
+  constructor() {}
+}
